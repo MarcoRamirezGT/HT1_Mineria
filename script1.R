6c7f32a7dd86772e47e5dc8b0ed57bb82af47390
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-01-28 18:59:26 -0600

    Update script1.R

---
+++ script1.R
@@ -0,0 +1,24 @@
+datos<-read.csv('movies.csv')
+summary(datos) #Resumen de los datos
+nrow(datos) #Cantidad de columnas
+ncol(datos) #Cantidad de columnas
+str(datos) #Estructura de los datos
+names(datos)
+
+
+#Vectores
+
+
+datos[,'id']
+datos[,'voteCount']
+datos[,'originalTitle']
+
+
+id<-datos[,'id']
+original_title<-datos[,'originalTitle']
+voteCount<-datos[,'voteCount']
+
+
+q3<-data.frame(id,original_title,voteCount)
+View(q3)
+
