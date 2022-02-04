2a3d7e157d58935d8880cc7f1c430e0d8048dd43
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-02-04 11:23:35 -0600

    Update script2.R

--- script2.R
+++ script2.R
@@ -41,6 +41,66 @@ ggplot(data=ask3f, aes(x=reorder(original_title,-presu) , y=presu,fill=original_
 
 
 
+
+
+#Pregunta 4.3
+
+datos[,'id']
+datos[,'voteCount']
+datos[,'originalTitle']
+
+
+id<-datos[,'id']
+Pelicula<-datos[,'originalTitle']
+voteCount<-datos[('voteCount')]
+
+
+q3<-data.frame(Pelicula,voteCount)
+ask3<-q3[order(q3$voteCount),]
+ask3f<-head(ask3,n=1)
+
+
+
+ggplot(data=ask3f, aes(x=reorder(Pelicula,-voteCount) , y=voteCount,fill=Pelicula)) +
+  geom_bar(stat="identity")+
+  
+  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1))+
+  labs(title="Pelicula con menos votos segun los usuarios", x="Peliculas", y="Votos")
+
+
+
+
+
+#Pregunta 4.7
+
+library(tidyverse)
+id<-datos[,'id']
+original_title<-datos[,'originalTitle']
+genres_amount<-datos[,'genresAmount']
+
+genre<-datos[,'genres']
+ingresos<-datos[,'revenue']
+
+
+ask6<-data.frame(original_title,genre,ingresos)
+ask6order<-ask6[rev(order(ask6$ingresos)),]
+ans6<-ask6order[!ask6order$ingresos=="",]
+ask6orderf<-head(ans6,n=5)
+
+data6<-ask6orderf
+datas<-data6 %>%
+  group_by(genre) %>%
+  tally()
+
+datas<-datas[order(-datas$n),]
+
+View(datas)
+genero<-datas$genre
+ggplot(data=datas, aes(x=genre, y=n,fill=genero)) +
+  geom_bar(stat="identity")+
+  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1))+
+  labs(title="Las películas de qué genero principal obtuvieron mayores ganancias", x="Genero", y="Ganancias")
+
 # 
 # barplot(height = ask3f$presu,names=ask3f$original_title,
 #         col=c('red','green','purple','blue','yellow'),
